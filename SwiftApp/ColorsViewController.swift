90e1b0c11f9dae1bcfa1521ddae17a05d5845e56
JuanRa Ramón Rodríguez Rosado <dragondr3w@gmail.com>
2019-10-20 12:44:30 +0200

    Implementada funcionalidad con tabla de colores

--- SwiftApp/ColorsViewController.swift
+++ SwiftApp/ColorsViewController.swift
@@ -8,12 +8,49 @@
 
 import UIKit
 
-class ColorsViewController: UIViewController {
+class ColorsViewController: UIViewController, CreateColorViewControllerDelegate, UITableViewDataSource {
+    
+    var names: [String]?
+    var colors: [UIColor]?
+    
+    @IBOutlet weak var colorsTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("ColorsViewController")
+        
+        names = Array()
+        colors = Array()
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let identifier = segue.identifier
+        if identifier == "CreateColor"{
+            let createColorViewController = segue.destination as! CreateColorViewController
+            createColorViewController.delegate = self
+        }
+    }
+    
+    func createColor(name: String, color: UIColor){
+        names!.append(name)
+        colors!.append(color)
+
+        self.colorsTableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names!.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "CellId")
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "CellId")
+        }
+        
+        cell?.backgroundColor = colors![indexPath.row]
+        cell?.textLabel?.text = names![indexPath.row]
+        
+        return cell!
+    }
     
 }
